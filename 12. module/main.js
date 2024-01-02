97a10e8b9746df55ea94a8a9e7722d8947d8bfa8
haeun <nhe0103@gamil.com>
2024-01-02 14:27:40 +0900

    second

---
+++ 12. module/main.js
@@ -0,0 +1,14 @@
+import * as counter from './couter.js'
+//import { increase,getCount } from './couter.js';
+// import increase  from './couter.js';
+// import {increase as increase1  } from "./couter.js";
+// as increase1로 이름을 바꾸겠다
+
+// increase1()
+counter.increase();
+counter.increase();
+counter.increase();
+counter.increase();
+
+
+console.log(counter.getCount);

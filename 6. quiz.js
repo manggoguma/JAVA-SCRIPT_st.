2192c6f2400c5b296b040d4ba0b3d27e16c63666
haeun <nhe0103@gamil.com>
2024-01-09 14:44:10 +0900

    plz

--- 6. quiz.js
+++ 6. quiz.js
@@ -1,5 +1,25 @@
 // 클래스를 베이스로한 객체지향 프로그래밍
 
 class Animal{
+    constructor(name,emoji){
+        this.name= name;
+        this.emoji = emoji;
+    }
     
 }
+
+Animal.prototype.printName = function() {
+    console.log(`${this.name} ${this.emoji}`);
+};
+/////////////////////////////////////////////////
+class Dog extends Animal{
+    constructor(owner){
+        super();
+        this.owner
+    }
+}
+
+
+
+const dog1 = new Dog('멍멍','🐶','하은');
+dog1.printName();
